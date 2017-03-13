3ffdb53999515df739ff1691c2abcc0a05173102
lucasasalmins <lucasasalmins@users.noreply.github.com>
2017-03-13 16:14:01 +0000

    Delete favnum.rb

--- favnum.rb
+++
@@ -1,5 +0,0 @@
-puts 'Hello there, what\'s your favourite number?'
-favnum = gets.chomp
-puts 'Cool! ' + favnum.to_s + ' is a cool number'
-bigger_num = favnum.to_i * 25
-puts 'But ' + bigger_num.to_s + ' is WAY BIGGER and BETTER!'
