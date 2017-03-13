a89a0d8334f26e7cf813e02b9bfaa967354109e6
lucasasalmins <lucasasalmins@users.noreply.github.com>
2017-03-13 16:10:52 +0000

    Delete apost.rb

--- apost.rb
+++
@@ -1,3 +0,0 @@
-puts 'now i\'m going to be a well paid coder, then an excellent CTO, then a visionary founder'
-puts 'now i\'m going to be a well paid coder, then an excellent CTO, then a visionary founder'
-puts 'now i\'m going to be a well paid coder, then an excellent CTO, then a visionary founder'
