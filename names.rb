17298f3d26a861113b87f0058e3d837c50bfb67a
Lucas Salmins <lucasasalmins@users.noreply.github.com>
2017-09-20 23:19:58 +0300

    Delete names.rb

--- names.rb
+++
@@ -1,8 +0,0 @@
-puts 'Hello there, what\'s your name?'
-Fname = gets.chomp
-puts 'Cool! and your middle name,' + Fname + ' ?'
-Mname = gets.chomp
-puts '...and your surname?'
-Sname = gets.chomp
-#this is stopping it from working for some reason. must be to do with the assignment of variables
-puts 'Hi ' + Fname + Mname + Sname '!' 
